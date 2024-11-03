bb4d2892f2be2b91737e14e62207f677f7a1b673
SwaggyBookshelf <87007414+SwaggyBookshelf@users.noreply.github.com>
2024-11-03 22:07:08 +0100

    fix for loading images and opening them in modal view

--- js/gallery-modal.js
+++ js/gallery-modal.js
@@ -14,6 +14,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const modalContainer = document.getElementById('modal-display');
     const modalImage = document.getElementById('modal-image');
 
+    loadJSON();
+
     document.querySelectorAll('.gallery-image').forEach(image => {
         image.addEventListener('click', openModal);
     });
@@ -111,4 +113,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
         e.stopPropagation();
     }
+
+    function createImageElement(image) {
+        const img = document.createElement('img');
+        console.log("AAAAAAAAAa")
+        img.src = image.scr;
+        img.alt = image.alt;
+        img.classList.add('grid-item', 'gallery-image');
+        img.addEventListener('click', function () { openModal(); });
+
+        const container = document.getElementById('main-gallery');
+        container.appendChild(img);
+    };
+
+    async function loadJSON() {
+        let response = await fetch("js/images.json");
+        console.log(response);
+        let data = await response.json();
+        console.log(data);
+
+        data.images.forEach(item => createImageElement(item));
+    };
 });
